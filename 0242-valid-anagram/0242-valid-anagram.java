c44317f002a716f16fb6686d100b3520bfdd29cf
wakharedevashree <144428308+wakharedevashree@users.noreply.github.com>
2026-01-19 23:24:31 +0530

    Time: 5 ms (58.03%), Space: 44.4 MB (79.14%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+          if(s.length()!=t.length())return false;
+          int f[]=new int[26];
+          for(int i=0;i<s.length();i++){
+            f[s.charAt(i)-'a']++;
+            f[t.charAt(i)-'a']--;
+          }
+          for(int count:f){
+            if(count!=0) return false;
+          }
+          return true;
+    }
+}
