1a79b3da95e67b688b72c570fbc281c2849c042f
wakharedevashree <144428308+wakharedevashree@users.noreply.github.com>
2026-02-16 20:32:12 +0530

    Time: 0 ms (100%), Space: 9.6 MB (6.41%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -8,12 +8,17 @@ public:
     }
     
     void push(int x) {
-        q2.push(x);
+        
         while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
-        swap(q1,q2);
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
     }
     
     int pop() {
